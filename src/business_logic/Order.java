95d63f2708a183970c171e82d370e175e2337f6b
zhenmingda <zhen.mingda@gmail.com>
2016-05-24 22:55:24 +0200

    test only121212

---
+++ src/business_logic/Order.java
@@ -0,0 +1,125 @@
+package business_logic;
+
+
+import dao.OrderDao;
+import database.ConnectionManager;
+import dto.OrderTo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * Created by Mingda Zhen on 2016/4/8.
+ *  version 1.0
+ * Order class invokes DAO and DTO to transfer data
+ */
+
+
+public class Order {
+    ConnectionManager db = new ConnectionManager();
+    private String recipeName;
+    private String price;
+    private String ingredient;
+    private String image;
+    private int dishID;
+    private String amount;
+
+
+    public Order(String recipeName, String ingredient, String price, String amount) {
+        this.recipeName = recipeName;
+        this.amount = amount;
+        this.price = price;
+        this.ingredient = ingredient;
+    }
+
+    public Order() {
+
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public int getDishID() {
+        return dishID;
+    }
+
+    public void setDishID(int dishID) {
+        this.dishID = dishID;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+// tranfer order data to order table in the database by DAO
+    public boolean toOrder(int tableID) {
+        List<OrderTo> judgeOrderTo = new LinkedList<>();
+        judgeOrderTo = new OrderDao(db).read(tableID);
+        OrderTo orderTo = new OrderTo(tableID, dishID, recipeName, ingredient, amount, price);
+        db.connect();
+        OrderDao orderDao = new OrderDao(db);
+        for (OrderTo i : judgeOrderTo) {
+            if (i.getDishID() == dishID) {
+                orderDao.update(orderTo);
+                return false;
+            }
+        }
+        orderDao.create(orderTo);
+        return true;
+    }
+    // get order data from order table in the database by DAO
+    public List<Order> getOrder(int tableNumber) {
+
+        List<Order> orderList = new LinkedList<>();
+        OrderDao orderDao = new OrderDao(db);
+        List<OrderTo> orderTo = orderDao.read(tableNumber);
+        for (OrderTo i : orderTo) {
+            orderList.add(new Order(i.getRecipeName(), i.getIngredient(), i.getPrice(), i.getAmount()));
+
+        }
+        return orderList;
+
+    }
+//delete order in the order table in database by DAO
+    public void delete(int tableNumber, String recipeName) {
+        OrderDao orderDao = new OrderDao(db);
+        OrderTo orderTo = new OrderTo();
+        orderTo.setTableID(tableNumber);
+        orderTo.setRecipeName(recipeName);
+        orderDao.delete(orderTo);
+    }
+}
---
+++ src/business_logic/Recipe.java
@@ -0,0 +1,114 @@
+package business_logic;
+
+import dao.RecipeDao;
+import database.ConnectionManager;
+import dto.RecipeTo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by dashu on 2016/4/1.
+ * version 1.0
+ * Recipe class invokes DAO and DTO to get recipe information
+ */
+public class Recipe {
+    ConnectionManager db = new ConnectionManager();
+    private String recipeName;
+    private String introduction;
+    private String price;
+    private String category;
+    private String image;
+    private int dishID;
+
+
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public int getDishID() {
+        return dishID;
+    }
+
+    public void setDishID(int dishID) {
+        this.dishID = dishID;
+    }
+
+
+
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    //read recipe by category from recipe table in database
+    public List<Recipe> readRecipeByCategory(String category) {
+        List<Recipe> recipeList = new LinkedList<Recipe>();
+
+        RecipeDao recipeDao = new RecipeDao(db);
+        List<RecipeTo> recipeToList = recipeDao.readRecipeByCategory(category);
+        for (RecipeTo i : recipeToList) {
+            Recipe recipe = new Recipe();
+            recipe.setDishID(i.getDishID());
+            recipe.setIntroduction(i.getIntroduction());
+            recipe.setCategory(i.getCategory());
+            recipe.setRecipeName(i.getRecipeName());
+            recipe.setPrice(i.getPrice());
+            recipe.setImage(i.getImage());
+            recipeList.add(recipe);
+        }
+
+        return recipeList;
+    }
+    //read recipe by name from recipe table in database
+    public Recipe readRecipeByName(String recipeName) {
+
+
+        RecipeDao recipeDao = new RecipeDao(db);
+       RecipeTo recipeTo = recipeDao.readRecipeByName(recipeName);
+
+            Recipe recipe = new Recipe();
+            recipe.setDishID(recipeTo.getDishID());
+            recipe.setIntroduction(recipeTo.getIntroduction());
+            recipe.setCategory(recipeTo.getCategory());
+            recipe.setRecipeName(recipeTo.getRecipeName());
+            recipe.setPrice(recipeTo.getPrice());
+            recipe.setImage(recipeTo.getImage());
+
+
+        return recipe;
+    }
+}
